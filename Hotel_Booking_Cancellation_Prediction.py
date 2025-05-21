14d362171154d58a48320a78a8af7bda18e97b05
Muhammad Rizky Indrawan <mochrizkyindrawan99@gmail.com>
2025-05-21 12:58:55 +0700

    Hotel_Booking_Cancellation_Prediction.py

--- Hotel_Booking_Cancellation_Prediction.py
+++ Hotel_Booking_Cancellation_Prediction.py
@@ -1,5 +1,9 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.ensemble import GradientBoostingClassifier
 import pickle
 
 # Judul Aplikasi
